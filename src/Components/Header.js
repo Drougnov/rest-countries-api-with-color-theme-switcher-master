ef48064f856cf8919facdfd7eab8975252cbe77a
RockyBaura <251drougnov@gmail.com>
2022-10-05 15:31:43 +0600

    Styled header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,12 @@
 export default function Header(){
     return(
-        <p>This is header.</p>
+        <header>
+            <a href="#" className="logo">Where in the world?</a>
+            <div className="toggle-theme">
+                <span><i class="fa-regular fa-moon"></i></span>
+                <span>Dark Mode</span>
+            </div>
+        </header>
     )
 }
+{/* <i class="fa-regular fa-sun"></i> */}
